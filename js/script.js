a1011b82ab2a02addf5e10edce282ad337d21dca
ByteEnchantress <151950206+ByteEnchantress@users.noreply.github.com>
2024-05-27 00:51:04 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing animation
+var typed = new Typed(".typing", {
+    strings:["","Ingeniería de Sistemas","9no semestre","Dev Web"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
